3a4815e5c90250f8288695d326566ce4eaffa32f
Olivia <oliviaclarkeedwards@gmail.com>
2023-05-18 14:31:16 +1200

    implement recursive bubble sort

--- newYearChaos.py
+++ newYearChaos.py
@@ -20,35 +20,33 @@
 #     return 
 
 
+
 def getNumberOfSwaps(line, swaps = 0):
-    #implement bubble sort 
-    # record number of swaps
+    n = len(line)
+    sorted = True
+    
+    for i in range(n - 1):
+        if line[i] > line[i + 1]:
+            line[i], line[i + 1] = line[i + 1], line[i]
+            swaps += 1
+            sorted = False
 
-    for i in range(len(line)):
-      value = line[i]
-      original_value = i + 1
-      if value > original_value + 2:
-          print("Too chaotic")
-          return
-      
-    for i in range(len(line) - 1):
-        if line[j] > line[j + 1]:
-                line[j], line[j + 1] = line[j + 1], line[j]
-                swaps += 1
-        
-        for j in range(len(line) - 1 - i):
-            if line[j] > line[j + 1]:
-                line[j], line[j + 1] = line[j + 1], line[j]
-                swaps += 1
-
-    print(swaps)
-    return 
+    if sorted:
+        return swaps
+    else:
+        swaps = getNumberOfSwaps(line[:n], swaps)
+        return swaps
             
+
+
         
-getNumberOfSwaps([1, 5, 2, 3, 4])      
-getNumberOfSwaps([1, 2, 3, 4, 5])
-getNumberOfSwaps([1, 2, 5, 3, 4])
-getNumberOfSwaps([1, 2, 5, 3, 7, 8, 6, 4])
+print(getNumberOfSwaps([1, 5, 2, 3, 4]))
+print(getNumberOfSwaps([1, 2, 3, 4, 5]))     
+print(getNumberOfSwaps([1, 2, 5, 3, 4]))     
+print(getNumberOfSwaps([1, 2, 5, 3, 7, 8, 6, 4]))        
+
+
+
 
 
 
