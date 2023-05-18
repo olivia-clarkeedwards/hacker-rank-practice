2b204c47a9c83e211af89c46503286fb1300b5b6
Olivia <oliviaclarkeedwards@gmail.com>
2023-05-18 15:31:59 +1200

    tidy and rewrite description

--- newYearChaos.py
+++ newYearChaos.py
@@ -1,25 +1,20 @@
-# take a list that contains mixed integers 1 to n where n is the size of the list 
-# if the value of a number in the list is more than two places away from its index + 1, return "too much chaos"
-# else if the value is greater than its index, add (value - 1) - current_index
+'''
 
-# def getNumberOfSwaps(line):
-#     bribes = 0
-    
-#     for index in range(len(line)):
-#         value = line[index]
-#         original_value = index + 1
-#         if value > original_value + 2:
-#             print("Too chaotic")
-#             return
-#         elif value > original_value:
-#             bribes += value - original_value
-            
-          
-    
-#     print(bribes)
-#     return 
+Description: Counts the minimum amount of swaps needed to reach the given list from a sorted list in ascending order. If any element is more than two swaps from its original position, prints "Too chaotic".Else, prints minimum number of swaps required. 
+Author: Olivia Clarke-Edwards
 
+'''
+ 
+def minimumBribes(line):
+    n = len(line)
 
+    for i in range(n):
+        if line[i] > i + 3:
+            print('Too chaotic')
+            return 
+        
+    print(getNumberOfSwaps(line))
+    return
 
 def getNumberOfSwaps(line, swaps = 0, memo = {}):
     n = len(line)
@@ -39,14 +34,11 @@ def getNumberOfSwaps(line, swaps = 0, memo = {}):
     else:
         memo[key] = getNumberOfSwaps(line[:n], swaps, memo)
         return memo[key]
-            
-
-
-        
-print(getNumberOfSwaps([1, 5, 2, 3, 4]))
-print(getNumberOfSwaps([1, 2, 3, 4, 5]))     
-print(getNumberOfSwaps([1, 2, 5, 3, 4]))     
-print(getNumberOfSwaps([1, 2, 5, 3, 7, 8, 6, 4]))        
+             
+minimumBribes([1, 5, 2, 3, 4])
+minimumBribes([1, 2, 3, 4, 5])     
+minimumBribes([1, 2, 5, 3, 4])     
+minimumBribes([1, 2, 5, 3, 7, 8, 6, 4])        
 
 
 
