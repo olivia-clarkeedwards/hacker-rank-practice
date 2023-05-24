2a44f6d3a9f20b8c55815e672374f11e408202c0
Olivia <oliviaclarkeedwards@gmail.com>
2023-05-24 14:56:29 +1200

    implement function to print item at front of the queue

--- twoStackQueue.py
+++ twoStackQueue.py
@@ -7,6 +7,21 @@ class Queue():
                  
     def dequeue(self):
         # if the queue is empty, return 
+        if self.size() <= 0:
+            return 
+        
+        top_item = self.stack.pop()
+
+        if self.size() == 0:
+            return top_item
+        
+        front_item = self.dequeue()
+
+        self.stack.append(top_item)
+
+        return front_item
+
+            
         # pop off top item and store 
         # if queue is empty, return popped item 
         # add top item back on the stack 
@@ -15,19 +30,29 @@ class Queue():
             
         
     def printFrontItem(self):
-        if self.size() != 0:
-          if not self.isEmpty(self.one):
-              for _ in range(len(self.one)):
-                  self.two.append(self.one.pop())
-          print(self.two[-1])
-        else:
-            print("Queue empty.")
+
+        # if the queue is empty, return 
+        if self.size() <= 0:
+            return
+        
+        top_item = self.stack.pop()
+
+        if self.size() == 0:
+            print(top_item) 
+        
+        self.printFrontItem()
+        self.stack.append(top_item)
+    
+        
 
     def isEmpty(self, stack):
         return stack == []
     
     def size(self):
         return len(self.stack)
+    
+    def toString(self):
+        return self.stack
 
         
         
@@ -37,7 +62,7 @@ twoStackQ = Queue()
 for _ in range(queryCount):
     
     q = input().split()
-    
+
     if q[0] == '1':
         twoStackQ.enqueue(int(q[1]))
     elif q[0] == '2':
@@ -45,8 +70,8 @@ for _ in range(queryCount):
     else:
         twoStackQ.printFrontItem()
         
-    
-    
+
+
     
     
     
