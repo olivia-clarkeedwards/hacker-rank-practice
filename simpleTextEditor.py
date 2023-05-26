1cfb223684f8b5758565ed33deb59f2ce42a7d3a
Olivia <oliviaclarkeedwards@gmail.com>
2023-05-26 16:16:35 +1200

    complete simple text editor class with methods to add, delete and print words

--- simpleTextEditor.py
+++ simpleTextEditor.py
@@ -1,15 +1,44 @@
+class SimpleTextEditor():
+    def __init__(self):
+        self.string = ''
+        self.last_move = []
+
+    def addString(self, string_to_append):
+        self.string += string_to_append
+        self.last_move.append([1, len(string_to_append)])
+
+    def delete(self, num_to_delete):
+        deleted_section = self.string[len(self.string) - num_to_delete:]
+        self.string = self.string[:len(self.string) - num_to_delete]
+        self.last_move.append([2, deleted_section])
+
+    def printChar(self, index_to_print):
+        print(self.string[index_to_print - 1])
+
+    def undoLastMove(self):
+        if len(self.last_move) == 0:
+            print('No moves to undo.')
+        else:
+            move_to_undo = self.last_move.pop()
+            if move_to_undo[0] == 1:
+                self.string = self.string[:len(self.string) - move_to_undo[1]]
+            else:
+                self.string += move_to_undo[1]
+
+
 def processQueries():
     queryCount = int(input())
+    s = SimpleTextEditor()
 
     for _ in range(queryCount):
         query = input().split()
         if query[0] == '1':
-            # append specified string to current string
+            s.addString(query[1])
         elif query[0] == '2':
-            # delete last specified number of letters
+            s.delete(int(query[1]))
         elif query[0] == '3':
-            # print the nth specified letter
+            s.printChar(int(query[1]))
         else:
-            # undo the last append or delete operation
+            s.undoLastMove()
 
 processQueries()
