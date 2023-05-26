bbaeeec77eed2a3dbad36b3d0c4c913f5bbdae07
Olivia <oliviaclarkeedwards@gmail.com>
2023-05-26 15:32:39 +1200

    add specification comments for query types

--- simpleTextEditor.py
+++ simpleTextEditor.py
@@ -3,6 +3,13 @@ def processQueries():
 
     for _ in range(queryCount):
         query = input().split()
-        print(query)
+        if query[0] == '1':
+            # append specified string to current string
+        elif query[0] == '2':
+            # delete last specified number of letters
+        elif query[0] == '3':
+            # print the nth specified letter
+        else:
+            # undo the last append or delete operation
 
 processQueries()
